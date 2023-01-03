a30f984ff8b20278dae082e647c88371f6266448
MrMean <morteza_hoseyni81@yahoo.com>
2023-01-03 19:40:10 +0330

    #0019 Read data from db;

---
+++ Database.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DWriterDetect
+{
+    internal class Database
+    {
+        public SQLiteDataReader readData(string tbName, string search = "1 = 1")
+        {
+			try
+			{
+				SQLiteCommand cmd = new SQLiteCommand($"SELECT * FROM {tbName} WHERE {search}");
+				SQLiteDataReader reader = cmd.ExecuteReader();
+
+				return reader;
+			}
+			catch (Exception err)
+			{
+				return null;
+				throw;
+			}
+        }
+    }
+}
